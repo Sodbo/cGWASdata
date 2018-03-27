35aca2f614521f1668ede24d643a102ee6542f03
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-27 16:20:10 +0700

    Move small functions to the utilities

---
+++ scripts/utilities.R
@@ -0,0 +1,44 @@
+#core functions
+
+#betas
+	
+.b=function(response,pred,S){
+	out=solve(S[pred,pred])%*%S[response,pred]
+	return(out)
+}
+
+# standart errors of betas
+
+.seb=function(response,pred,S,N){
+	sigma_joint=(S[response,response]-t(.b(response,pred,S=S))%*%S[response,pred])
+	#sigma_joint=S[response,response]
+	out=as.vector(sigma_joint)*solve(S[pred,pred])/(N-length(pred)-1)
+	
+	out=diag(out)
+	out=sqrt(out)
+	return(out)
+}
+
+# 
+.manova=function(response,pred,S,N){
+	sigma_joint=(S[response,response]-t(.b(response,pred,S=S))%*%S[response,pred])
+	Fst=((S[response,response]-as.vector(sigma_joint))/as.vector(sigma_joint))*((N-length(pred)-1)/length(pred))
+	return(Fst)
+}
+
+#T statistics (not T^2)
+
+.t=function(response,pred,S,N){
+	out=.b(response=response,pred=pred,S=S)/.seb(response=response,pred=pred,S=S,N=N)
+	return(out)
+}
+
+ReadCovariates=function(covariates,path2data,suffix=".txt"){
+	Nprd=length(covariates)
+	prd=covariates[1]
+	for (prd in covariates){
+	if (!exists(prd)){
+		eval(text=paste(prd,"<-read.table(paste(path2data,prd,suffix,sep=\"\"),header=T,stringsAsFactors=F)",sep=""))
+	}
+	}
+}
