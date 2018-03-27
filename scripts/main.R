e00a68d625bd8a6a0aa9668943e2d95ad50877c8
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-27 19:15:25 +0700

    Modified main.R script

--- scripts/main.R
+++ scripts/main.R
@@ -4,15 +4,15 @@ source('../code/exact_cGWAS.R')
 
 # Set the responce
 
-response <- 'lysoPC.a.C20:4'
+response <- 'lysoPC.a.C20.4'
 
 # Set the covariates
 
-covariates <- c('lysoPC.a.C20:3')
+covariates <- c('lysoPC.a.C20.3')
 
 # Loading correlation matrix for 151 metabolites
 
-cor_mat <- read.table('../data/20171207_corr_matrix.txt')
+cor_matrix <- read.table('../data/20171207_corr_matrix.txt')
 
 # Loading information about SNPs, where column 'var_1785' 
 # contains variance of the SNP
@@ -20,18 +20,23 @@ cor_mat <- read.table('../data/20171207_corr_matrix.txt')
 snp_info <- data.table::fread('../data/30_SNP_information.txt',
 	data.table = FALSE)
 
+all_varg <- snp_info$varg_1785
+
+names(all_varg) <- snp_info$SNP
+
+
 exact_cGWAS(
 	CovM = cor_matrix,
-	all_varg = snp_info$var_1785,
+	all_varg = all_varg,
 	response = response,
 	N = 1785,
 	covariates = covariates,
-	cn_b = "beta_SNP",
+	cn_b = "beta",
 	cn_snp = "SNP",
-	cn_se = "se_SNP",
+	cn_se = "se",
 	output_threshold=1,
 	good_snps = NULL,
-	all_CR = 1,
-	correction = TRUE,
+	#all_CR = 1,
 	path_uGWAS = '../data/uGWAS_snps_from_paper'
+
 )
