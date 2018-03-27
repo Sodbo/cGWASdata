73899c0172867d69be6b238dbd368a9e535a940d
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-27 19:15:12 +0700

    Refactored. Now SE=NA is in output

--- scripts/exact_cGWAS.R
+++ scripts/exact_cGWAS.R
@@ -4,7 +4,7 @@
 
 # The main function. It uses uGWAS results stored in .RData format. 
 # CovM - covariance matrix of response vairable and covariates
-# all_varg - a numeric vector with variances of studied SNPs
+# all_varg - a named numeric vector with variances of studied SNPs
 # response - a character, name of response variable
 # N - sample size
 # covariates - a character vector with list of covariates
@@ -20,141 +20,181 @@ exact_cGWAS <- function(
 	all_varg,
 	response,
 	N,
-	covariates=NULL,
+	covariates = NULL,
 	cn_b = "beta_SNP",
 	cn_snp = "SNP",
 	cn_se = "se_SNP",
-	output_threshold=1e-6,
+	output_threshold = 1e-6,
 	good_snps = NULL,
 	all_CR = 1,
-	correction = TRUE,
-	path_uGWAS = NULL){
+	correction = FALSE,
+	path_uGWAS = './'){
 	
 	#checking the data
 
-	cat("Load data...","\n")
+	cat("Loading data ...","\n")
 
-	Nprd <- length(covariates)
+	# In this list sum stats for responce and covariates will be collected
 
-	#eval(text=paste("snps=",response",[,cn_snp]",sep=""))
+	sum_stats <- list()
 
-	snps=names(all_varg)
+	# Reading sum_stats and pulling into the list
 
-	resp_pred<-c(response,covariates)
-	
-    if (is.null(path_uGWAS)){
-        prd <- resp_pred[1]
-        for (prd in resp_pred){
-            load(paste(path_uGWAS,prd,".RData",sep=""))
-        }
-	} else {
-        prd=resp_pred[1]
-        for (prd in resp_pred){
-            load(paste(path_uGWAS,prd,".RData",sep=""))
-        }
-    }
-        
-	cat("Filtering for NAs and searching overlapping SNPs...","\n")
-	prd=resp_pred[1]
-	for (prd in resp_pred){
-        eval(parse(text=paste("ind=which(is.na(",prd,"[,cn_b]))",sep="")))
-        if (length(ind)>0){
-            #eval(parse(text=paste(prd,"=",prd,"[!is.na(",prd,"[,cn_b]),]",sep="")))
-            eval(parse(text=paste(prd,"=",prd,"[-ind,]",sep="")))
-        }
+	for(trait in c(response,covariates)) {
+
+		trait_file <- paste0(path_uGWAS,'/',trait, '.txt')
+
+		sum_stats[[length(sum_stats)+1]] <- read.table(trait_file, 
+			head=TRUE, 
+			stringsAsFactors=FALSE)
+
+		rm(trait)
+
 	}
-	
-	prd=resp_pred[1]
-	for (prd in resp_pred){
-		eval(parse(text=paste("snps_prd=",prd,"[,cn_snp]",sep="")))
-		snps=intersect(snps,snps_prd)
+
+	names(sum_stats) <- c(response, covariates)
+
+	all_traits <- names(sum_stats)
+
+	cat("Filtering for NAs ...","\n")
+
+	for(trait in all_traits){
+
+		sum_stats[[trait]] <- na.omit(sum_stats[[trait]])
+
+		rm(trait)
+
 	}
-	
-	if (!is.null(gut_snps)) snps=intersect(gut_snps,snps)
-	#if (!is.null(all_CR)) snps=intersect(names(all_CR),snps)
-	
-	if (is.null(snps)|length(snps)==0){
+	  
+	cat("Searching for overlapping SNPs ...","\n")
+
+	snps_over <- sum_stats[[1]][,cn_snp]
+
+	for(trait in all_traits) {
+
+		snps_over <- intersect(snps_over, sum_stats[[trait]][,cn_snp])
+
+		rm(trait)
+
+	}
+
+	# Intersect with good SNPs
+
+	if (!is.null(good_snps)) 
+
+		snps_over <- intersect(good_snps, snps_over)
+
+	if (is.null(snps_over) | length(snps_over)==0){
+
 		warning("No overlapping SNPs between inputs")
 		opt <- options(show.error.messages=FALSE) 
 		on.exit(options(opt)) 
 		stop() 
-	#stop("No overlapping SNPs between inputs")
+
 	}	
 	
-	snps=unique(snps)
-	Nsnps=length(snps)
-		
-	all_varg=all_varg[snps]
-	#all_CR=all_CR[snps]
-	prd=resp_pred[1]
-	for (prd in resp_pred){
-		eval(parse(text=paste("index=match(snps,",prd,"[,cn_snp])",sep="")))
-		eval(parse(text=paste(prd,"=",prd,"[index,]",sep="")))
+	snps <- unique(snps_over)
+
+	rm(snps_over)
+
+	all_varg <- all_varg[snps]
+
+	for(trait in all_traits){
+
+		keep_snps <- match(sum_stats[[trait]][,cn_snp],snps, nomatch = FALSE)
+
+		sum_stats[[trait]] <- sum_stats[[trait]][keep_snps,]
+
+		rm(trait)
+
 	}
-	
+
+	Nsnps <- length(snps)
+
 	cat(Nsnps,"overlapping SNPs in total...","\n")
 	
-	if (is.null(covariates)|length(covariates)==0){
+	if (is.null(covariates) | length(covariates) == 0){
 	
-		cat("No covariates were specified. No cGWAS was performed...","\n")
+		cat("No covariates were specified. No cGWAS was performed ...","\n")
+		cat("Returnign uGWAS sum_stats for the ", names(sum_stats)[1],"\n")
 		
-		out=array(NA,c(Nsnps,4))
-		colnames(out)=c("b","se","chi2","Pval")
-		rownames(out)=snps
+		out <- data.frame(
+			b = sum_stats[[1]][,cn_b], 
+			se = sum_stats[[1]][,cn_se],
+			chi2 = (sum_stats[[1]][,cn_b] / sum_stats[[1]][,cn_se])^2
+			)
 		
-		prd=resp_pred[1]
-		eval(parse(text=paste("out[,\"b\"]<-",prd,"[,cn_b]",sep="")))
-		eval(parse(text=paste("out[,\"se\"]<-",prd,"[,cn_se]",sep="")))
-		out[,"chi2"]<-(out[,"b"]/out[,"se"])^2
+		rownames(out) <- snps
+
+		# Pval is calculated in the very end after GC
 		
-		snps=snps[!is.na(out[,"chi2"])]
-		out=out[!is.na(out[,"chi2"]),]
-		
-	} else{
+	} else {
 	
-		#creating of S matrix
-		S=array(NA,c(Nprd+2,Nprd+2))
-		colnames(S)=rownames(S)=c(response,covariates,"g")
-		S[resp_pred,resp_pred] <- CovM[resp_pred,resp_pred]
+		# Creating S matrix
+
+		source('../code/utilities.R')
+
+		Nprd <- length(all_traits)
+
+		S <- array(NA, c(Nprd + 1, Nprd + 1))
+
+		colnames(S) <- rownames(S) <- c(names(sum_stats), "g")
+
+		S[all_traits, all_traits] <- as.matrix(CovM[all_traits, all_traits])
+	
+		# calculation
+
+		out <- array(NA,c(Nsnps, 4))
+
+		colnames(out) <- c("b","se","chi2","Pval")
+
+		rownames(out) <- snps
 		
-		#calculation
-		out=array(NA,c(Nsnps,4))
-		colnames(out)=c("b","se","chi2","Pval")
-		rownames(out)=snps
+		# beta matrix
+		betas <- array(NA, c(Nsnps, length(all_traits)))
+		rownames(betas) <- snps
+		colnames(betas) <- all_traits
 		
-		#beta matrix
-		betas=array(NA,c(Nsnps,length(resp_pred)))
-		rownames(betas)=snps
-		colnames(betas)=resp_pred
-		
-		for (prd in resp_pred){
-			eval(parse(text=paste("betas[,prd]<-",prd,"[,cn_b]",sep="")))
+		for (prd in all_traits){
+
+			betas[,prd] <- sum_stats[[prd]][,cn_b]
+
 		}
 		cat("Starting cGWAS...","\n")
 		
-		i=1
-		pb <- txtProgressBar(style=3)
-		for (i in (1:Nsnps)){
-			setTxtProgressBar(pb, value=i/Nsnps)
-			S["g","g"]<-varg<-all_varg[i]
-			prd=resp_pred[1]
-			for (prd in resp_pred){
-				b1=betas[i,prd]
-				S["g",prd]<-S[prd,"g"]<-b1*varg
-			}
-			b<-.b(response=1,pred=2:(Nprd+2),S=S)["g",]
-			if (length(all_CR)==1){
-                se<-.seb(response=1,pred=2:(Nprd+2),S=S,N=N*all_CR)["g"]
-            } else{
-                se<-.seb(response=1,pred=2:(Nprd+2),S=S,N=N*all_CR[i])["g"]
-            }
+		# pb <- txtProgressBar(style=3)
 
-			out[i,"b"]<-b
-			out[i,"se"]<-se
-			#out[i,"chi2"]<-(b/se)^2
+		for (i in 1:Nsnps){
+
+			# setTxtProgressBar(pb, value = i/Nsnps)
+
+			S["g","g"] <- varg <- all_varg[i]
+
+			for (prd in all_traits){
+
+				b1 <- betas[i,prd]
+
+				S["g",prd] <- S[prd,"g"] <- b1*varg
+
+			}
+
+			out[i,"b"] <-.b(response=1, pred=2:(Nprd+1),S=S)["g",]
+
+            out[i,"se"] <-.seb(response=1, pred=2:(Nprd+1),S = S, N = N*all_CR[i])["g"]
+
+			# print(paste('########## ', i ,' ###################'))
+
 		}
-		out[,"chi2"]<-(out[,"b"]/out[,"se"])^2
-		close(pb)
+
+		out[,"chi2"] <- (out[,"b"]/out[,"se"])^2
+
+		print(out)
+
+		out <- as.data.frame(out)
+
+
+		
+		#close(pb)
 		
 		#out=as.data.frame(out)
 		#out=cbind(SNP=snps,out)
@@ -163,37 +203,46 @@ exact_cGWAS <- function(
 		cat("cGWAS was calculated for",dim(out)[1],"SNPs","\n")
 	}	
 	
-	#output
-	output=list()
+	# Combinig output 
+
+	output <- list()
 	
-	output$gc_lambda=median(out[,"chi2"],na.rm=T)/qchisq(0.5,1,lower.tail=F)
+	output$gc_lambda <- median(out$chi2,na.rm=T) / qchisq(0.5, 1, lower.tail=F)
+	
 	cat("GC Lambda of all",dim(out)[1],"SNPs is",output$gc_lambda,"\n")
 	
 	if (correction){
-		cat("Correcting...","\n")
-		out[,"chi2"]=out[,"chi2"]/output$gc_lambda
-		output$status="corrected for GC lambda"
+		cat("Performing genomic control correction ...","\n")
+		
+		out$chi2 <- out$chi2 / output$gc_lambda
+		output$status="Corrected for GC lambda"
+
 	} else{
+
 		output$status="Not corrected for GC lambda"
+
 	}
 	
-	out[,"Pval"]=pchisq(out[,"chi2"],1,lower.tail=F)
+	out$Pval <- pchisq(out$chi2, 1, lower.tail = FALSE)
 		
-	output$response=response
-	output$covariates=paste(covariates,collapse=",")
+	output$response <- response
+
+	output$covariates <- paste(covariates, collapse=";")
 	
-	cat("Filtering results using threshold =",output_threshold,"\n")
-	
-	snps=snps[out[,"Pval"]<=output_threshold]
-	out=out[out[,"Pval"]<=output_threshold,]
+	cat("Filtering results using threshold = ",output_threshold,"\n")
+
+	out <- out[out$Pval <=output_threshold ,]
 	
 	cat(dim(out)[1],"SNPs with p-value <=",output_threshold,"\n")
 	
 	#colnames(out)=c("b","se","chi2","Pval")
-	output$results=out
-	output$snps=snps
+	
+	output$results <- out
+	
+	output$snps <- rownames(out)
 	
 	cat("Finished. Enjoy=)","\n")
+	
 	return(output)
 }
 
