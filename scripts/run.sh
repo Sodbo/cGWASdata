0db36a6a2feaaf93ebc0973e9646cb2d681d1422
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-08-29 16:43:30 +0700

    Minor change

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,14 +10,6 @@ echo "Running cGAS analysis using GGM distances"
 echo "Running main_GGM.R"
 Rscript code/main_GGM.R
 
-echo "Creating Figure 1"
-echo "Running Figure_1.R"
-Rscript code/Figure_1.R
-
-echo "Creating Figure 2"
-echo "Running Figure_2.R"
-Rscript code/Figure_2.R
-
 echo "Creating Supplementary Table 1A"
 echo "Running Suppl.table.1A.R"
 Rscript code/Suppl.table.1A.R
@@ -29,3 +21,11 @@ Rscript code/Suppl.table.1B.R
 echo "Creating Supplementary Table 2"
 echo "Running Suppl.table.2.R"
 Rscript code/Suppl.table.2.R
+
+echo "Creating Figure 1"
+echo "Running Figure_1.R"
+Rscript code/Figure_1.R
+
+echo "Creating Figure 2"
+echo "Running Figure_2.R"
+Rscript code/Figure_2.R
